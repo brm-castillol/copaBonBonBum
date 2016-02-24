492d8c0e965cbc963a20d05a7c8364d5f058335a
lady castillo <esteffany.castillo@brm.com.co>
2016-02-24 10:37:05 -0500

    actualiza

---
+++ publication/js/switch.js
@@ -0,0 +1,5 @@
+ <script>
+        var Switchery = require('switchery');
+        var elem = document.querySelector('.js-switch');
+        var init = new Switchery(elem,{ color: '#69150a'});
+ </script>
